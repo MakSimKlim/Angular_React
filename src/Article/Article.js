8aa040af7c3b19a7882e5027b910d96ab514dc7f
MakSimKlim <83maxomus@mail.ru>
2024-11-26 18:20:27 +0500

    init

---
+++ src/Article/Article.js
@@ -0,0 +1,15 @@
+function Article()
+{
+    return( 
+    <article>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, repellat voluptatem labore quidem quasi minus impedit. Magnam esse, aut nostrum veniam eum adipisci minus nihil eos, ducimus ipsum, quod perferendis.
+    </p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae expedita numquam saepe doloremque veritatis ducimus eveniet maiores tempora. Voluptatem in unde, vero vel sed quasi debitis laudantium nemo odio maxime.
+    Assumenda omnis animi praesentium distinctio dolorum quisquam repudiandae. Hic, quaerat tenetur, libero modi iure cum inventore, sunt distinctio blanditiis labore dolores similique facere? Eveniet rem iure exercitationem delectus. Nisi, delectus.
+    Optio eius officia animi quis, in maxime beatae ducimus at numquam rerum quos cupiditate officiis labore sequi? Dolor, sint, facilis qui non dolorem itaque, aliquam nisi assumenda sapiente vero ex!          
+    </p>
+  </article>
+    );
+}
+
+export default Article;
