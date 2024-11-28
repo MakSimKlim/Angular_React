5aee1e9f97325a40d010d07a9c2d078b27ddaecf
MakSimKlim <83maxomus@mail.ru>
2024-11-28 18:06:17 +0500

    Navigation menu DONE

--- intro/src/components/app/App.js
+++ intro/src/components/app/App.js
@@ -2,11 +2,25 @@
 import './App.css';
 
 import Header from '../header/Header';
+import Nav from '../nav/Nav';
+import Power from '../power/Power';
 import Article from '../article/Article';
 import Footer from '../footer/Footer';
-import Power from '../power/Power';
+
 
 function App() {
+
+  // навигационное меню
+  let nav = 
+  {
+    "Главная"   : "/index",
+    "Новости"   : "/new",
+    "Магазин"   : "/shop",
+    "О нас"     : "/about",
+    "Контакты"  : "/contacts",
+
+  }
+
   return (
     <div className="App">
       
@@ -14,6 +28,8 @@ function App() {
       description="This is my first React App. 
       This Phase passedusing parameters 
       over 'PROPS'"/>
+
+      <Nav navigation={nav}/>
 
       <Power a={2} n={8}/>
 
